f1d4eb394f4df164b586748ead2b1c7e559d45a3
davisraimon <davis.raimon@fractal.ai>
2021-08-31 09:12:06 +0000

    initial commit

---
+++ sensor/house_info.py
@@ -0,0 +1,13 @@
+class HouseInfo:
+
+    def __init__(self, data):
+        self.data = data
+
+    def get_data_by_area(self, field, rec_area = 0):
+        field_data = []
+        for record in self.data:
+            if rec_area == 0:
+                field_data.append(record[field])
+            elif rec_area == int(record[field]):
+                field_data.append(record[field])
+        return field_data
